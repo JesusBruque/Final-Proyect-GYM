2807598c49704b8c2806be04313e5702d376256d
NayraBH <91415065+NayraBH@users.noreply.github.com>
2022-02-22 20:42:03 +0000

    feat: db model user and role (#1)

--- src/api/models/user.py
+++ src/api/models/user.py
@@ -2,10 +2,13 @@ from api.models.db import db
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    created_at = db.Column(db.DateTime, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
+    first_name = db.Column(db.String(80), nullable=False)
+    last_name = db.Column(db.String(80), nullable=False)
+    phone = db.Column(db.String(80), nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False, default=True)
-
 
     def __repr__(self):
         return '<User %r>' % self.id
@@ -13,5 +16,8 @@ class User(db.Model):
     def serialize(self):
         return {
             "id": self.id,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
             "email": self.email,
+            "phone": self.phone,
         }
