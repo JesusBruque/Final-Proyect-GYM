1af65593166995dddb293810f026290803966ad2
Luis Piñero Quintana <lpineroq@gmail.com>
2022-02-24 20:27:15 +0000

    Commit 1

--- src/front/js/Layout/Footer/Footer.jsx
+++ src/front/js/Layout/Footer/Footer.jsx
@@ -1,10 +1,31 @@
 import React, { Component } from "react";
 
 export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
-		</p>
-	</footer>
+  <footer className="footer mt-auto py-3 text-center">
+    <div class="collapse" id="navbarToggleExternalContent">
+      <div class="bg-dark p-4">
+        <h5 class="text-white h4">Collapsed content</h5>
+        <span class="text-muted">Toggleable via the navbar brand.</span>
+      </div>
+    </div>
+    <nav class="navbar navbar-dark bg-dark">
+      <div class="container-fluid">
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarToggleExternalContent"
+          aria-controls="navbarToggleExternalContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      </div>
+    </nav>
+    <p>
+      Made with <i className="fa fa-heart text-danger" /> by{" "}
+      <a href="http://www.4geeksacademy.com">4Geeks Academy</a>
+    </p>
+  </footer>
 );
--- src/front/js/Layout/Navbar/Navbar.jsx
+++ src/front/js/Layout/Navbar/Navbar.jsx
@@ -1,19 +1,46 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
-			</div>
-		</nav>
-	);
+  return (
+    <navbar>
+      <div className="container-fluid">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarTogglerDemo03"
+          aria-controls="navbarTogglerDemo03"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <a className="navbar-brand" href="#">
+          Navbar
+        </a>
+        <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <Link to="/">
+                <span className="navbar-brand mb-0 h1">React Boilerplate</span>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/demo">
+                <button className="btn btn-primary">
+                  Check the Context in action
+                </button>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </navbar>
+  );
 };
