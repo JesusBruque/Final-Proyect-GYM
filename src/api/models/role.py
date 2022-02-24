cea8fbf66e7e7c211a32238f2024b7214ff99c69
NayraBH <91415065+NayraBH@users.noreply.github.com>
2022-02-24 19:52:06 +0000

    feat: user professional register back (#3)
    * feat: user professional register back
    
    * fix: role relationship with user

--- src/api/models/role.py
+++ src/api/models/role.py
@@ -2,7 +2,7 @@ from api.models.db import db
 
 class Role(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    role = db.Column(db.String(20), nullable=False)
+    role = db.Column(db.String(20), nullable=False, unique=True)
 
     def __repr__(self):
         return '<Role %r>' % self.id
--- src/api/models/user.py
+++ src/api/models/user.py
@@ -1,14 +1,18 @@
+import datetime
 from api.models.db import db
+from api.models.role import Role
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    created_at = db.Column(db.DateTime, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     first_name = db.Column(db.String(80), nullable=False)
     last_name = db.Column(db.String(80), nullable=False)
     phone = db.Column(db.String(80), nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False, default=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('role.id'))
+    role = db.relationship(Role)
 
     def __repr__(self):
         return '<User %r>' % self.id
