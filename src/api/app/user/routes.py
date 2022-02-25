852e705b7e16547b117878839c604aa0ac197764
JesusBruque <bruke3_voleyutrera@hotmail.com>
2022-02-25 15:35:37 +0000

    refactor

--- src/api/app/user/routes.py
+++ src/api/app/user/routes.py
@@ -1,11 +1,11 @@
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.app.user_professional.controler import register_user, login_user, get_user_by_id
+from api.app.user.controler import register_user, login_user, get_user_by_id
 from flask_jwt_extended import get_jwt_identity
 from flask_jwt_extended import jwt_required
 
 users = Blueprint('users', __name__)
 
-@users.route('/register/professional', methods=['POST'])
+@users.route('/register', methods=['POST'])
 def create_user():
     body = request.get_json()
     new_user = register_user(body)
