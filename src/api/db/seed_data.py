a540680354d75dcc3717fb81d955586477e3a78f
JesusBruque <bruke3_voleyutrera@hotmail.com>
2022-02-26 10:44:31 +0000

    feat: refactor and seed

---
+++ src/api/db/seed_data.py
@@ -0,0 +1,46 @@
+data = {
+    "Role":[
+        {
+            "id": 1,
+            "role_name": "customer"
+        },
+        {
+            "id": 2,
+            "role_name": "trainer"
+        },
+        {
+            "id": 3,
+            "role_name": "physio"
+        }
+    ],
+    "User":[
+        {
+            "id":1,
+            "email": 'lencericardo@mail.com',
+            "password": "123456",
+            "first_name": "Ricardo",
+            "last_name": "loquesea",
+            "phone": "123456789",
+            "role_id": 1
+            
+        },
+         {
+            "id":2,
+            "email": 'batman@mail.com',
+            "password": "123456",
+            "first_name": "Bruce",
+            "last_name": "Wayne",
+            "phone": "987654321",
+            "role_id": 2
+        },
+        {
+            "id":3,
+            "email": 'chan@mail.com',
+            "password": "167824",
+            "first_name": "Jackie",
+            "last_name": "Chan",
+            "phone": "145628399",
+            "role_id": 3
+        }
+    ]
+}
