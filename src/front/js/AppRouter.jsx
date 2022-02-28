15e225b0b39e0d71bf0b2b89187ffd666afc7470
JesusBruque <bruke3_voleyutrera@hotmail.com>
2022-02-28 17:01:29 +0000

    feat: user profile

--- src/front/js/AppRouter.jsx
+++ src/front/js/AppRouter.jsx
@@ -5,6 +5,7 @@ import injectContext from "./store/appContext";
 
 //Views
 import Home from "./views/Home/Home.jsx";
+import Account from "./views/Account/Account.jsx";
 
 //Layout
 import Layout from "./Layout/Layout.jsx";
@@ -23,6 +24,9 @@ const AppRouter = () => {
 						<Switch>
 							<Route exact path="/">
 								<Home />
+							</Route>
+							<Route exact path="/account">
+								<Account />
 							</Route>
 							<Route>
 								<h1>Not found!</h1>
--- src/front/js/Layout/Layout.jsx
+++ src/front/js/Layout/Layout.jsx
@@ -22,7 +22,7 @@ const Layout = (props) => {
 }
 
 Layout.propTypes = {
-    children: PropTypes.func
+    children: PropTypes.object
 }
 
 export default Layout;
