f5e493685c3e59a74123948d5d7f34f4c838df11
Luis Piñero Quintana <lpineroq@gmail.com>
2022-03-03 19:43:26 +0000

    Commit 1.2

--- src/front/js/views/Home/Admin-Pannel/Admin-Pannel-Menu.jsx
+++ src/front/js/views/Home/Admin-Pannel/Admin-Pannel-Menu.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import "./Admin-Pannel-Styles/Admin-Pannel-Menu.css";
 
-const AdminPannelNotifications = () => {
+const AdminPannelMenu = () => {
   return <div></div>;
 };
 
-export default AdminPannelNotifications;
+export default AdminPannelMenu;
