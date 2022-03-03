1215621b0501e33b3688d76f5c1a0709d009670b
Luis Piñero Quintana <lpineroq@gmail.com>
2022-03-03 20:31:48 +0000

    Commit 1.2

--- src/front/js/views/Home/Admin-Pannel/Admin-Pannel-Menu.jsx
+++ src/front/js/views/Home/Admin-Pannel/Admin-Pannel-Menu.jsx
@@ -2,7 +2,25 @@ import React from "react";
 import "./Admin-Pannel-Styles/Admin-Pannel-Menu.css";
 
 const AdminPannelMenu = () => {
-  return <div></div>;
+  return (
+    <div className="btn-group-vertical">
+      <button type="button" className="btn btn-danger">
+        Customers
+      </button>
+      <button type="button" className="btn btn-warning">
+        Trainers
+      </button>
+      <button type="button" className="btn btn-success">
+        Physio
+      </button>
+      <button type="button" className="btn btn-warning">
+        Notifications
+      </button>
+      <button type="button" className="btn btn-success">
+        Timetable
+      </button>
+    </div>
+  );
 };
 
 export default AdminPannelMenu;
