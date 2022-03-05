d73b10c9c83382d9225d13e1e53ee344be38af5f
Luis Piñero Quintana <92811994+LPineroQ@users.noreply.github.com>
2022-03-05 11:31:26 +0100

    Front/navbar footer (#11)
    * Commit 1
    
    * Commit 1
    
    * Commit 2
    
    * Commit 3
    
    * Final commit

--- src/front/js/Layout/Navbar/Navbar.jsx
+++ src/front/js/Layout/Navbar/Navbar.jsx
@@ -1,19 +1,51 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import "./Navbar.css";
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
-			</div>
-		</nav>
-	);
+  return (
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarTogglerDemo03"
+          aria-controls="navbarTogglerDemo03"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <a className="navbar-brand" href="#">
+          GYM APP
+        </a>
+        <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">
+                Login
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">
+                Únete a nosotros
+              </a>
+            </li>
+          </ul>
+          <form className="d-flex">
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Buscar"
+              aria-label="Search"
+            ></input>
+            <button className="btn btn-warning busqueda" type="submit">
+              Buscar
+            </button>
+          </form>
+        </div>
+      </div>
+    </nav>
+  );
 };
