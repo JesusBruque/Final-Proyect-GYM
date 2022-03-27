f8f9f1835fc32f5bca6dffa9961e84d111b7d0eb
JesusBruque <bruke3_voleyutrera@hotmail.com>
2022-03-27 10:55:52 +0000

    feat: navbar

--- src/front/js/Layout/Layout.jsx
+++ src/front/js/Layout/Layout.jsx
@@ -1,18 +1,19 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import { Navbar } from "./Navbar/Navbar.jsx";
-import { Footer } from "./Footer/Footer.jsx";
+//import { Navbar } from "./Navbar/Navbar.jsx";
+//import { Footer } from "./Footer/Footer.jsx";
+import { Navbar1 } from "./Navbar/Navbar1.jsx";
 
 const Layout = (props) => {
   return (
     <>
       <div>
-        <Navbar />
+        <Navbar1 />
       </div>
       <div>{props.children}</div>
       <div>
-        <Footer />
+
       </div>
     </>
   );
