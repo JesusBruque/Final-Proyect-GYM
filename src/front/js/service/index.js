8756ebfe0c6d8bb0c79d744aae89cfb793f4b1ac
Luis Piñero Quintana <lpineroq@gmail.com>
2022-04-27 17:40:59 +0000

    Commit 1 y final

--- src/front/js/service/index.js
+++ src/front/js/service/index.js
@@ -1,6 +1,6 @@
 //export const URL = process.env.BACKEND_URL;
 export const URL =
-  "https://3001-jesusbruque-finalproyect-8ei61idyiic.ws-eu40.gitpod.io";
+  "https://3001-jesusbruque-finalproyec-9me2e00hm72.ws-eu42.gitpod.io";
 
 export const getToken = () => {
   const tokenUser = localStorage.getItem("token");
